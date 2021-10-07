504fadb0d9db869cbd762f20c938b5723e2895b3
jalaanil <67024154+jalaanil@users.noreply.github.com>
2021-10-07 22:44:51 +0200

    Add files via upload

---
+++ tf_infra/setenv.cmd
@@ -0,0 +1,14 @@
+REM Set the path for the tools
+
+set PATH=%PATH%;d:\cloud\azure\tools;
+
+
+REM set azure parameters 
+REM IMPORTANT NOTE : Set commands below  - values are not enclosed in double quotes
+
+set ARM_SUBSCRIPTION_ID=b483ceb2-d286-400a-bcae-7d8fd01b0d75
+set ARM_TENANT_ID=d68a238a-c112-4e92-82be-5f67eb3da666
+
+
+set ARM_CLIENT_ID=40cb5fa5-6890-4bb8-8076-92dbaf67b109
+set ARM_CLIENT_SECRET=M.l-PD_2w-UsHEL81Yb-JZ~JHv~1JBO8vg
